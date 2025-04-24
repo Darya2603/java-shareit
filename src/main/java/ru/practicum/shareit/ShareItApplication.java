9ba9f154a1f6daf98b9acf714ca06323b0539ba8
PiTri <burova.dv@yandex.ru>
2025-04-24 19:59:14 +0300

    commit-2

--- src/main/java/ru/practicum/shareit/ShareItApplication.java
+++ src/main/java/ru/practicum/shareit/ShareItApplication.java
@@ -1,12 +1,14 @@
 package ru.practicum.shareit;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItApplication {
+	private static final int PORT = 8080;
 	public static void main(String[] args) {
+		Tomcat tomcat = new Tomcat();
 		SpringApplication.run(ShareItApplication.class, args);
-		System.out.println("Приложение запущено на http://localhost:8080");
 	}
 }
