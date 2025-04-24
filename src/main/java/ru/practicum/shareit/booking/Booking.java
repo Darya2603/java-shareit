54bb0e859ad3eef98d4c7a15f480b06e1ad35298
PiTri <burova.dv@yandex.ru>
2025-04-24 22:13:40 +0300

    commit-2

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -21,11 +21,11 @@ public class Booking {
 
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime StartDate;
+    private LocalDateTime startDate;
 
     @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private LocalDateTime EndDate;
+    private LocalDateTime endDate;
 
     @ManyToOne
     @JoinColumn(name = "item_id", nullable = false)
